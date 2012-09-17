2e3dc07f8a9126278a697e642165905a137816e3
Andrew <sevirol@i.ua>
2012-09-17 18:03:50 +0300

    interface

--- db/migrate/20120607210421_devise_create_users.rb
+++ db/migrate/20120607210421_devise_create_users.rb
@@ -5,6 +5,13 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
 
+      #
+      t.string   :name
+      t.string   :nickname, :null => false, :default => ""
+      t.datetime :birthday
+      t.string   :country
+      t.string   :city
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(:version => 20120607210421) do
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "name"
+    t.string   "nickname",               :default => "", :null => false
+    t.datetime "birthday"
+    t.string   "country"
+    t.string   "city"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
